3e8d37266fb782be870e6b6017dc871391a2fb5e
Ryan Breeden <ryan@ryanbreeden.com>
2018-06-30 14:06:21 -0400

    solution 1

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -5,8 +5,17 @@
 // and punctuation in determining if the string is a palindrome.
 // --- Examples:
 //   palindrome("abba") === true
+//   palindrome("abcba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  // RAB SOLUTION
+  var index2 = str.length - 1;
+  for ( let index = 0; index < str.length / 2; index++){
+    if(str[index] !== str[index2]) return false;
+    index2--;
+  }
+  return true;
+}
 
 module.exports = palindrome;
