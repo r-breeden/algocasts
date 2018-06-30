8e022cf67ef5f6baa9dbc150c645b6898c4eb8e7
Ryan Breeden <ryan@ryanbreeden.com>
2018-06-30 08:23:06 -0400

    solution 2 for reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,19 +6,24 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+// SOLUTION 1
+// function reverse(str) {
+//   var str2 = str.split("").reverse().join("");
+//   return str2;
+// }
+
+// // SOLUTION 2
+// function reverse(str) {
+//   var reversed = '';
+//   for (let char of str) {
+//     reversed = char + reversed;
+//   }
+//   return reversed;
+// }
+
+// SOLUTION 3
 function reverse(str) {
-  //solution 1
-  // var str2 = str.split("").reverse().join("");
-  // return str2;
-
-  //solution 2
-  var reversed = '';
-  for (let char of str) {
-    reversed = char + reversed;
-  }
-  return reversed;
-
-
+  
 }
 
 module.exports = reverse;
