b49807ba4021cd8037448a93f0cda7e2ebf1b7f4
Ryan Breeden <ryan@ryanbreeden.com>
2018-07-03 14:49:06 -0400

    solution 2

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,22 +8,34 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {
-  if (n === 0) return 0;
+// RAB SOLUTION
+// function reverseInt(n) {
+//   if (n === 0) return 0;
   
-  var isNeg = n < 0; 
-  number = n;
+//   var isNeg = n < 0; 
+//   number = n;
 
-  if(isNeg){
-    number = number * -1;
-  }
+//   if(isNeg){
+//     number = number * -1;
+//   }
 
-  number = number.toString().split("").reverse().join("");
+//   number = number.toString().split("").reverse().join("");
 
-  if(isNeg){
-    return number * -1;
-  }
-  return number * 1;
+//   if(isNeg){
+//     return number * -1;
+//   }
+//   return number * 1;
+// }
+
+//SOLUTION 2
+function reverseInt(n) {
+  var number = n
+  .toString()
+  .split("")
+  .reverse()
+  .join("");
+
+  return parseInt(number) * Math.sign(n);
 }
 
 module.exports = reverseInt;
