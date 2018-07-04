2b5274cf03769f0a6a3346e120910f729efc98ca
Ryan Breeden <ryan@ryanbreeden.com>
2018-07-04 19:02:45 -0400

    solution 1

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  var map = {};
+
+  for (let char of str){
+    map[char] = map[char] + 1 || 1;
+  }
+
+  var mostFrequentValue = 0;
+  var mostFrequentKey;
+  var keysList = Object.keys(map);
+
+  for (let key of keysList) {
+    if(mostFrequentValue < map[key]){
+      mostFrequentValue = map[key];
+      mostFrequentKey = key;
+    }
+  }
+
+  return mostFrequentKey;
+}
 
 module.exports = maxChar;
