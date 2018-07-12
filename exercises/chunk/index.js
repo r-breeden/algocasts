0af893ea66b4b504b23e12c717fba47aba6a7e7c
Ryan Breeden <ryan@ryanbreeden.com>
2018-07-12 07:39:45 -0400

    solution 2

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,18 +8,36 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {
-  var chunkedArray = [];
+// RAB SOLUTION
+// function chunk(array, size) {
+//   var chunkedArray = [];
 
-  for (let i = 0; i < array.length; i = i + size) {
-    var chunk = [];
-    for (let k = i; k < i + size && k < array.length; k++) {
-      chunk.push(array[k]);
+//   for (let i = 0; i < array.length; i = i + size) {
+//     var chunk = [];
+//     for (let k = i; k < i + size && k < array.length; k++) {
+//       chunk.push(array[k]);
+//     }
+//     chunkedArray.push(chunk);
+//   }
+
+//   return chunkedArray;
+// }
+
+// SOLUTION 2
+function chunk(array, size) {
+  const chunked = [];
+
+  for (let element of array) {
+    const last = chunked[chunked.length - 1];
+
+    if (!last || last.length === size) {
+      chunked.push([element]);
+    } else {
+      last.push(element);
     }
-    chunkedArray.push(chunk);
   }
 
-  return chunkedArray;
+  return chunked;
 }
 
 module.exports = chunk;
