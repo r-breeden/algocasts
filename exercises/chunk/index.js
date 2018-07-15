d248c3cee44119e44d9da3c7fd56abde89265059
Ryan Breeden <ryan@ryanbreeden.com>
2018-07-15 11:08:34 -0400

    solution 3

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -24,17 +24,30 @@
 // }
 
 // SOLUTION 2
+// function chunk(array, size) {
+//   const chunked = [];
+
+//   for (let element of array) {
+//     const last = chunked[chunked.length - 1];
+
+//     if (!last || last.length === size) {
+//       chunked.push([element]);
+//     } else {
+//       last.push(element);
+//     }
+//   }
+
+//   return chunked;
+// }
+
+// SOLUTION 3
 function chunk(array, size) {
   const chunked = [];
+  let index = 0;
 
-  for (let element of array) {
-    const last = chunked[chunked.length - 1];
-
-    if (!last || last.length === size) {
-      chunked.push([element]);
-    } else {
-      last.push(element);
-    }
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
   }
 
   return chunked;
