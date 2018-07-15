e53f3e4c0f24876f264985e4d0dabe5a85580bdb
Ryan Breeden <ryan@ryanbreeden.com>
2018-07-15 11:44:34 -0400

    solution 1

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,39 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// RAB SOLUTION
+function anagrams(stringA, stringB) {
+  let string1 = stringA.replace(/[^\w]/g, " ").toLowerCase();
+  const string2 = stringB.replace(/[^\w]/g, " ").toLowerCase();
+
+  const string1Map = {};
+  const string2Map = {};
+ 
+  for (let el of string1){
+    if (!string1Map.el) {
+      string1Map[el] = 1;
+    } else {
+      string1Map[el]++;
+    }
+  }
+
+   for (let el of string2){
+    if (!string2Map.el) {
+      string2Map[el] = 1;
+    } else {
+      string2Map[el]++;
+    }
+  }
+
+  if ( Object.keys(string1Map).length !== Object.keys(string2Map).length) return false;
+
+  for (let prop in string1Map) {
+    if (!string2Map[prop] || string2Map[prop] !== string1Map[prop]) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 module.exports = anagrams;
